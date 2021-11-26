b112e3fc8d7fc05a28b4466d7bf494259a53ea2d
selwynng117 <selwynng123@gmail.com>
2021-11-26 20:23:57 +0800

    navigation_bar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+// finds all .collapsible in the html and then adds another class collapsible --expanded right after .collapsible class
+
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
